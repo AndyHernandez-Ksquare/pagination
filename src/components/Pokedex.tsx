8fe341c04e7b695e963aef0a69117d37c3e60c25
AndyHernandez-Ksquare <Andy.Hernandez@itksquare.edu.mx>
2023-05-01 11:35:27 -0400

    Added dynamic card colors.
    Need to work on form visuals and responsiveness

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -6,12 +6,12 @@ type Props = {
   pokemon: IInfoPokemon[];
   searchValue: string | number;
   searchURL: string;
-  currentPage: number;
-  displayPages: number[];
-  totalPages: number;
-  goToPage: (page: number) => void;
-  goToNextPage: () => void;
-  goToPreviousPage: () => void;
+  currentPage?: number;
+  displayPages?: number[];
+  totalPages?: number;
+  goToPage?: (page: number) => void;
+  goToNextPage?: () => void;
+  goToPreviousPage?: () => void;
 };
 
 const Pokedex = ({
@@ -28,31 +28,25 @@ const Pokedex = ({
   // Generate an array of page numbers to display
 
   const list = pokemon?.map((value) => (
-    <Pokemon
-      searchURL={searchURL}
-      searchValue={searchValue}
-      name={value.name}
-      url={value.url}
-      key={value.name}
-    />
+    <Pokemon name={value.name} url={value.url} key={value.name} />
   ));
 
   return (
     <section className="pokedex">
       {list}
-      {totalPages > 1 && (
+      {totalPages! > 1 && (
         <div className="pagination">
           <button className="pages" onClick={goToPreviousPage}>
             {"<<"}
           </button>
-          {displayPages.map((page) => {
+          {displayPages!.map((page) => {
             const isActiveClass = currentPage === page ? "active" : "";
 
             return (
               <button
                 key={page}
                 onClick={() => {
-                  goToPage(page);
+                  goToPage!(page);
                 }}
                 className={`${isActiveClass} pages`}
               >
--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -3,28 +3,33 @@ import PokemonTypes from "./PokemonTypes";
 import "../Pokemon.css";
 
 type Props = {
-  name?: string;
+  name: string;
   url: string;
-  searchValue?: string | number;
-  searchURL?: string;
 };
 
-const Pokemon = ({ url, searchValue, searchURL }: Props) => {
+const Pokemon = ({ url }: Props) => {
   const { order, sprite, types, generation, height, weight, name } =
     useFetchPokemonDetails(url);
-  // console.log(generation);
+
   return (
-    <section className="pokemon-container">
-      <h5>{name}</h5>
-      <section>
-        <PokemonTypes types={types} />
-        <h6 style={{ margin: 0 }}>No. {order}</h6>
-        <span>Height {height}</span>
-        <span>Weight {weight}</span>
-        <span>{generation}</span>
-        <figure>
-          <img src={sprite} alt={name} />
-        </figure>
+    <section className={`pokemonContainer ${types[0]} ${types[0]}Card`}>
+      <section className="nameAndOrder">
+        <h6 style={{ margin: 0 }}>#{order}</h6>
+        <h5>{name}</h5>
+      </section>
+      <PokemonTypes types={types} />
+
+      <section className="pokemonMiddle">
+        <section className="pokemonDescription">
+          <span>Height {height}</span>
+          <span>Weight {weight}</span>
+          <span>{generation}</span>
+        </section>
+        <section>
+          <figure>
+            <img src={sprite} alt={name} />
+          </figure>
+        </section>
       </section>
     </section>
   );
